5fb0f69b1722d528cdb771213880cce42a7f2c66
hcyvan <navych@126.com>
2015-05-25 13:58:45 +0800

    update

---
+++ configure.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+import os
+import sys
+fileList=os.listdir(os.getcwd())
+# change the scripts to excutable state, and create to soft link to them
+if (len(sys.argv)==1):
+	for file in fileList:
+		if(file[-3:]==".py" and file!="configure.py"):
+			os.system("chmod 755 "+file)
+			os.system("ln -s "+file+" "+file[:-3])
+
+# re move the soft link to the scripts
+if (len(sys.argv)==2 and sys.argv[1]=="-d"):
+	for file in fileList:
+		if(file[-3:]==".py" and file!="configure.py"):
+				os.system("rm "+file[:-3])
+
