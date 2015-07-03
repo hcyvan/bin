7cb7211d728e23304848b322eebd824a9925754e
hcyvan <navych@126.com>
2015-07-03 19:26:06 +0800

    update:

--- smtp.sh
+++ smtp.sh
@@ -8,9 +8,12 @@
 
 
 MAIL=/home/navy/mail
-file=${MAIL}/$(date +%Y%m%d%H%M%S)
+file=${MAIL}/$(date +%Y%m%d%H%M%S).eml
 touch ${file}
 echo -e "From: Navy <navych@126.com>\nTo:\nCc:\nBcc:\nSubject:\nReply-To: Navy <navych@126.com>\n" > ${file}
 vim ${file}
 msmtp -t < ${file}
-mv ${file} ${MAIL}/outbox/
+if [ $? -eq 0 ];then
+	mv ${file} ${MAIL}/outbox/
+fi
+
