3a7f1b8f572a79772e045bc5397fe4ee255a239a
Navy Cheng <navych@126.com>
2015-08-15 15:29:55 +0800

    Improve: register.py: use moudle argparse to manage the script arguments
    Use argparse moudle to make the arguments more standard. Remove unnessasarry
    check point because the fault will cause exception which will stop the
    process during running time.
    
    Signed-off-by: Navy Cheng <navych@126.com>

--- register.py
+++ register.py
@@ -1,18 +1,8 @@
-'''
-This is a python script to regist this directory to PATH, change the other scripts
-in this directory executable and create soft link without extend name for 
-them. Do not change this script to executable to prevent misuse.
-
-+ *python ./register* : Regist this directory and make other script executable
-+ *python ./register -d* : Delete the soft links. 
- 
-'''
+#! /usr/bin/python3
 import os
 import sys
 import re
-
-exts=set([r'.py',r'.sh'])
-paths=set(os.getenv('PATH').split(':'))
+import argparse
 
 ## Grep a *fileName* line-by-line by *regep*
 def grepfl(fileName,regep):
@@ -24,33 +14,51 @@ def grepfl(fileName,regep):
                 if pattern.search(line):
                         return True
         return False
-                
-# If os.getcwd() is not in PATH, Add it to .bashrc. The path will not
-# be added twice.
-if os.getcwd() not in paths:
-        # Judge whether does "~/.bashrc" exist
-        bashrc=os.path.join(os.getenv('HOME'),'.bashrc')
-        if os.path.exists(bashrc):
+
+# Add *path* to the PATH
+def add_to_PATH(path):
+        PATH=set(os.getenv('PATH').split(':'))
+        bashrc=os.getenv('HOME')+'.bashrc'
+        if path not in PATH:
                 commit='\n# Add by %s\n'%os.path.realpath(sys.argv[0])
-                shell='export PATH=$PATH:%s\n'%os.getcwd()
+                shell='export PATH=$PATH:%s\n'%path
                 if not grepfl(bashrc, r'%s'%commit.strip()):
-                        fd=open(bashrc,'a+')
+                        fd=open(bashrc, 'a+')
                         fd.write(commit+shell)
                         fd.close()
                         print("ADD:\n"+commit+shell+'\nTO '+bashrc)
 
-# Chomd 755 and create soft link for scripts 
-files=os.listdir(os.getcwd())
-for fname in files:
-        parts=os.path.splitext(fname)
-        if (parts[1] in exts):
-                if (len(sys.argv)==1):
-                        # Change the scripts to excutable state
-                        # and create to soft link to them
-                        os.system('chmod 755 '+fname)
-                        os.system('ln -s '+fname+' '+parts[0])
-                if (len(sys.argv)==2 and sys.argv[1]=='-d'):
-                        # Remove the soft link to the scripts
-                        os.system('rm '+parts[0])
-                        os.system('chmod -x '+fname)
+def install(files):
+        for f in files:
+                os.system('chmod 755 '+f)
+                os.system('ln -s '+f+' '+os.path.splitext(f)[0])
+def clean(files):
+        for f in files:
+                os.system('rm '+os.path.splitext(f)[0])
+                os.system('chmod -x '+f)
+
+                        
+if __name__ == '__main__':
+        describe="\
+        This is a python script to regist this directory to PATH, change \
+        the other scripts in this directory executable and create soft link \
+        without extend name for them. Do not change this script to executable \
+        to prevent misuse. -h or --help for usage"
+
+        parser = argparse.ArgumentParser(description=describe)
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("-c", "--clean", action="store_true")
+        group.add_argument("-i", "--install", action="store_true")
+        args = parser.parse_args()
+
+        files=set(os.listdir(os.getcwd()))-set([__file__, 'README.md','.git'])
+
+        if args.clean:
+                print('...clean...')
+                clean(files)
+
+        else:
+                print('...install...')
+                add_to_PATH(os.getcwd())
+                install(files)
 
